a33ec2e339166dd6a510d203afec5d48cea0f4eb
Jay George <github@jaygeorge.co.uk>
2021-07-23 12:05:44 +0200

    Champagne Club -- Add Curator section

--- champagne-club.php
+++ champagne-club.php
@@ -28,18 +28,30 @@
 
     <?php /* Need tabindex="-1" to lock the tab focus here when using Skip to Content */?>
     <main tabindex="-1" id="main">
-        <header class="image-with-text">
-            <div class="image-with-text__text">
-                <h1><span>the</span> champagne <br/>club</h1>
+        <article>
+            <header class="image-with-text">
+                <div class="image-with-text__text">
+                    <h1><span>the</span> champagne <br/>club</h1>
+                </div>
+                <img src="/img/1.jpg" width="3456" height="2304" alt="Some alt text here" class="image-with-text__image">
+                <button class="image-with-text__arrow">
+                    <?php include 'img/svg/down-arrow.php'; ?>
+                </button>
+                <div class="image-with-text__question-box" style="background: hsl(168deg 100% 5%);">
+                    <p>Access unique bottles from exclusive vineyards—delivered to your door every month.</p>
+                </div>
+            </header>
+            <div class="article">
+                <h2>Meet Edward, your bespoke wine curator</h2>
+                <p>Edward was born in London and raised in Chelsea in the ‘Swinging Sixties’. After prep school in Oxford and a north London grammar school, headed into the booming City of London of the 1980’s. By the turn of the millennium and after spells in New York City and Mionte Carlo he turned to the wine business.</p>
+ 
+                <p>What had started as a twice yearly run to Champagne for his city friends morphed into a business importing and distributing Champagne in the UK Europe the USA and Asia.</p>
+  
+                <p>By the age of 50 it was time for a change and Edward left his base in Mayfair and headed to his family home in a mountain village in Granada for a supposedly quieter life.</p>
             </div>
-            <img src="/img/1.jpg" width="3456" height="2304" alt="Some alt text here" class="image-with-text__image">
-            <button class="image-with-text__arrow">
-                <?php include 'img/svg/down-arrow.php'; ?>
-            </button>
-            <div class="image-with-text__question-box" style="background: hsl(168deg 100% 5%);">
-                <p>Access unique bottles from exclusive vineyards—delivered to your door every month.</p>
-            </div>
-        </header>
+            <img src="/img/edward.jpg" alt="Edward" />
+            <div class="caption">Edward, our expert wine curator</div>
+        </article>
     </main>
 
     <?php include '_inc/footer.php'; ?>
