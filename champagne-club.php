57e80016323fe7603ffd916f647d5d46946dc8a9
Jay George <github@jaygeorge.co.uk>
2021-07-23 12:25:02 +0200

    Champagne Club -- correct Curator section HTML and CSS

--- champagne-club.php
+++ champagne-club.php
@@ -41,17 +41,25 @@
                     <p>Access unique bottles from exclusive vineyards—delivered to your door every month.</p>
                 </div>
             </header>
-            <div class="article">
-                <h2>Meet Edward, your bespoke wine curator</h2>
-                <p>Edward was born in London and raised in Chelsea in the ‘Swinging Sixties’. After prep school in Oxford and a north London grammar school, headed into the booming City of London of the 1980’s. By the turn of the millennium and after spells in New York City and Mionte Carlo he turned to the wine business.</p>
- 
-                <p>What had started as a twice yearly run to Champagne for his city friends morphed into a business importing and distributing Champagne in the UK Europe the USA and Asia.</p>
-  
-                <p>By the age of 50 it was time for a change and Edward left his base in Mayfair and headed to his family home in a mountain village in Granada for a supposedly quieter life.</p>
-            </div>
-            <img src="/img/edward.jpg" alt="Edward" />
-            <div class="caption">Edward, our expert wine curator</div>
-        </article>
+            <div class="u-above-ctas">
+                <div class="article">
+                    <div class="article__text">
+                        <h2>Meet Edward, your bespoke wine curator</h2>
+                        <p>Edward was born in London and raised in Chelsea in the ‘Swinging Sixties’. After prep school in Oxford and a north London grammar school, headed into the booming City of London of the 1980’s. By the turn of the millennium and after spells in New York City and Mionte Carlo he turned to the wine business.</p>
+        
+                        <p>What had started as a twice yearly run to Champagne for his city friends morphed into a business importing and distributing Champagne in the UK Europe the USA and Asia.</p>
+        
+                        <p>By the age of 50 it was time for a change and Edward left his base in Mayfair and headed to his family home in a mountain village in Granada for a supposedly quieter life.</p>
+                    </div>
+                    <div class="article__image">
+                        <figure>
+                            <img src="/img/edward.jpg" alt="Edward" />
+                            <figcaption>Edward, our expert wine curator</figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </article>
+        </div>
     </main>
 
     <?php include '_inc/footer.php'; ?>
