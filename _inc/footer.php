d072811995825a64d3c48d4aa6ed26fbac8a8dac
Jay George <github@jaygeorge.co.uk>
2021-07-26 22:19:04 +0200

    Add footer with form -- progress

--- _inc/footer.php
+++ _inc/footer.php
@@ -20,33 +20,5 @@
             <?php $nav_name = 'Contact'; include '_inc/nav-links.php'; ?>
         </ul>
     </nav>
-    <form method="post" name="Sign up">
-        <div>
-            <label>
-                First Name
-            </label>
-            <input size="1" type="text">
-        </div>
-        <div>
-            <label>
-                Surname
-            </label>
-            <input size="1" type="text" placeholder="First Name">
-        </div>
-        <div>
-            <label>
-                Email
-            </label>
-            <input size="1" type="email">
-        </div>
-        <div>
-            <label>
-                Password
-            </label>
-            <input size="1" type="email">
-        </div>
-        <button type="submit">
-            Sign up
-        </button>
-    </form>
+    <?php include '_inc/sign-up-form.php'; ?>
 </footer>
