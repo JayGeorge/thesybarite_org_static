ee8b68ee711a97c330d11f9e00800578106d2af0
Jay George <github@jaygeorge.co.uk>
2021-07-26 22:26:24 +0200

    Tidy forms a bit

---
+++ _inc/sign-up-form.php
@@ -0,0 +1,29 @@
+<form class="form" method="post" name="Sign up">
+    <div>
+        <label>
+            First Name
+        </label>
+        <input size="1" type="text">
+    </div>
+    <div>
+        <label>
+            Surname
+        </label>
+        <input size="1" type="text" placeholder="First Name">
+    </div>
+    <div>
+        <label>
+            Email
+        </label>
+        <input size="1" type="email">
+    </div>
+    <div>
+        <label>
+            Password
+        </label>
+        <input size="1" type="email">
+    </div>
+    <button type="submit">
+        Sign up
+    </button>
+</form>
