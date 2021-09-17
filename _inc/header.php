e6ba4e4c49c31e4409d78f8102760cdf7066f2e5
Jay George <github@jaygeorge.co.uk>
2021-09-17 12:14:22 +0100

    Menu Menu -- finish desktop draft

--- _inc/header.php
+++ _inc/header.php
@@ -31,26 +31,42 @@
                 <a class="js__megaMenuInitiate" href="/<?php echo strtolower(str_replace(' ', '-', $nav_name));?>.php"><?php echo $nav_name; ?></a>
                 <div class="c-mega-menu-container">
                     <div class="c-mega-menu">
-                        <ul>
-                            <li><a href="">Arts & Culture</a></li>
-                            <li><a href="">Beauty & Wellbeing</a></li>
-                            <li><a href="">Best of Britain</a></li>
-                            <li><a href="">Create Your Own</a></li>
-                            <li><a href="">Fashion & Style</a></li>
-                            <li><a href="">Food & Drink</a></li>
-                            <li><a href="">Lifestyle & Adventure</a></li>
-                            <li><a href="">Parties & Events</a></li>
-                            <li><a href="">Private Stays</a></li>
-                            <li><a href="">Property</a></li>
-                            <li><a href="">Sport</a></li>
-                            <li><a href="">Technology</a></li>
-                            <li><a href="">Travel</a></li>
-                            <li><a href="">Virtual</a></li>
-                            <li><a href="">Yachts, Cars & Jets</a></li>
-                        </ul>
-                        <ul>
-                            <li><a href="">Editors Picks</a></li>
-                        </ul>
+                        <h2 class="c-mega-menu__full-width">Handcrafted, Luxury Experiences</h2>
+                        <h3><?php include 'img/svg/hot.php'; ?> What’s Hot Now</h3>
+                        <div class="c-mega-menu__grid">
+                            <ul class="c-mega-menu__full-width">
+                                <li>
+                                    <img src="/img/experiences/silverstone/silverstone.jpg" alt="Silverstone" />
+                                    <strong><a href="">Formula 1 Luxury Stay At Silverstone</a></strong>
+                                </li>
+                                <li>
+                                    <img src="/img/experiences/six-senses-ibiza/six-senses-ibiza.jpg" alt="Silverstone" />
+                                    <strong><a href="">Europe’s Hottest New Hotel — Six Senses Ibiza</a></strong>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li><a href="">Arts & Culture</a></li>
+                                <li><a href="">Beauty & Wellbeing</a></li>
+                                <li><a href="">Best of Britain</a></li>
+                                <li><a href="">Create Your Own</a></li>
+                                <li><a href="">Fashion & Style</a></li>
+                                <li><a href="">Food & Drink</a></li>
+                                <li><a href="">Lifestyle & Adventure</a></li>
+                                <li><a href="">Parties & Events</a></li>
+                            </ul>
+                            <ul>
+                                <li><a href="">Private Stays</a></li>
+                                <li><a href="">Property</a></li>
+                                <li><a href="">Sport</a></li>
+                                <li><a href="">Technology</a></li>
+                                <li><a href="">Travel</a></li>
+                                <li><a href="">Virtual</a></li>
+                                <li><a href="">Yachts, Cars & Jets</a></li>
+                            </ul>
+                            <ul class="c-mega-menu__full-width">
+                                <li><a href="">View All Experiences <?php include 'img/svg/arrow-right.php'; ?></a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </li>
@@ -87,11 +103,13 @@
             ?>
             <li><?php include 'img/svg/search.php'; ?></li>
 
-            <div class="btn">
-                <a href="/champagne-club-logged-in.php">
-                    Login
-                </a>
-            </div>
+            <li>
+                <div class="btn">
+                    <a href="/champagne-club-logged-in.php">
+                        Login
+                    </a>
+                </div>
+            </li>
         </ul>
     </nav>
 </header>
