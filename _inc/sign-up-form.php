09ea627e4584906eb0b86df5fa2900999f08bdf0
Jay George <github@jaygeorge.co.uk>
2021-10-07 12:06:21 +0200

    Forms -- replace .form with .c-form

--- _inc/sign-up-form.php
+++ _inc/sign-up-form.php
@@ -1,25 +1,25 @@
-<form class="form form--hidden-labels" method="post" name="Sign up">
+<form class="c-form c-form--hidden-labels" method="post" name="Sign up">
     <div class="c-site-footer__form-with-agreement">
         <div class="c-site-footer__form-with-agreement__form">
-            <p class="form__notes">
+            <p class="c-form__notes">
                 Sign up to enjoy exclusive member benefits
             </p>
-            <div class="form-cols">
-                <div class="form-cols__col">
+            <div class="c-form-cols">
+                <div class="c-form-cols__col">
                     <label>
                         First Name
                     </label>
                     <input size="1" type="text" placeholder="First Name">
                 </div>
-                <div class="form-cols__col">
+                <div class="c-form-cols__col">
                     <label>
                         Surname
                     </label>
                     <input size="1" type="text" placeholder="Surname">
                 </div>
             </div>
-            <div class="form-cols form-cols--1">
-                <div class="form-cols__col">
+            <div class="c-form-cols form-cols--1">
+                <div class="c-form-cols__col">
                     <label>
                         Email
                     </label>
@@ -32,7 +32,7 @@
                 Sign up
             </button>
             <input type="checkbox" checked name="form_fields[field_5d5d369]" id="form-field-field_5d5d369">
-            <label class="form-label-show" for="form-field-field_5d5d369">I agree to The Sybarite’s <a href="https://thesybarite.org/privacy-policy/">privacy policy</a></label>
+            <label class="c-form-label-show" for="form-field-field_5d5d369">I agree to The Sybarite’s <a href="https://thesybarite.org/privacy-policy/">privacy policy</a></label>
         </div>
     </div>
 </form>
