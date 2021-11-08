32650c2f32565b53f56260d666d758206df6e27b
Jay George <github@jaygeorge.co.uk>
2021-11-08 09:01:38 +0000

    Update imagery

--- art-club.php
+++ art-club.php
@@ -65,24 +65,24 @@
                     <p><a href="" class="c-link-button">Learn more</a></p>
                 </div>
                 <figure class="c-gallery__item">
-                    <img src="/img/clubs/art-club/gallery/ajarb-ategwa/1.png" alt="t" />
-                    <figcaption>Sans Titre</figcaption>
+                    <img src="/img/clubs/art-club/gallery/ajarb-ategwa/early-morning-selfie.png" alt="t" />
+                    <figcaption>Early morning selfie</figcaption>
                 </figure>
                 <figure class="c-gallery__item">
-                    <img src="/img/clubs/art-club/gallery/ajarb-ategwa/2.png" alt="t" />
-                    <figcaption>Mon Combat</figcaption>
+                    <img src="/img/clubs/art-club/gallery/ajarb-ategwa/am-i-looking-good.png" alt="t" />
+                    <figcaption>Am I looking good</figcaption>
                 </figure>
                 <figure class="c-gallery__item">
-                    <img src="/img/clubs/art-club/gallery/ajarb-ategwa/4.png" alt="t" />
-                    <figcaption>Brousses imaginaires</figcaption>
+                    <img src="/img/clubs/art-club/gallery/ajarb-ategwa/am-almost-done.png" alt="t" />
+                    <figcaption>Am almost done</figcaption>
                 </figure>
                 <figure class="c-gallery__item">
-                    <img src="/img/clubs/art-club/gallery/ajarb-ategwa/5.png" alt="t" />
-                    <figcaption>Sans tête</figcaption>
+                    <img src="/img/clubs/art-club/gallery/ajarb-ategwa/yaounde.png" alt="t" />
+                    <figcaption>Yaounde</figcaption>
                 </figure>
                 <figure class="c-gallery__item">
-                    <img src="/img/clubs/art-club/gallery/ajarb-ategwa/3.png" alt="t" />
-                    <figcaption>Brousses imaginaires</figcaption>
+                    <img src="/img/clubs/art-club/gallery/ajarb-ategwa/douala.png" alt="t" />
+                    <figcaption>Douala</figcaption>
                 </figure>
             </div>
             <h2 class="c-eyebrow-heading">Modern Life</h2>
@@ -95,14 +95,78 @@
                 </div>
                 <figure class="c-gallery__item">
                     <img src="/img/clubs/art-club/gallery/hamid-nortey/build-an-empire-leave-a-legacy.jpg" alt="t" />
-                    <figcaption>Kyomu</figcaption>
+                    <figcaption>Build an empire, leave a legacy</figcaption>
                 </figure>
                 <figure class="c-gallery__item">
                     <img src="/img/clubs/art-club/gallery/hamid-nortey/deep-summer.jpg" alt="t" />
-                    <figcaption>Congressman John Lewis</figcaption>
+                    <figcaption>Deep Summer is when laziness finds respectability</figcaption>
                 </figure>
             </div>
             <div class="u-width-max-1">
+                <?php $filename = 'plugins/photoswipe'; include '_inc/_stylesheet.php'; ?>
+                <script type="module">
+// Include Lightbox 
+import PhotoSwipeLightbox from '/js/plugins/photoswipe.esm.js';
+
+const lightbox = new PhotoSwipeLightbox({
+  // may select multiple "galleries"
+  gallery: '#gallery--simple',
+
+  // Elements within gallery (slides)
+  children: 'a',
+
+  // Include PhotoSwipe Core
+  // and use absolute path (that starts with http(s)://)
+  pswpModule: 'https://thesybarite-static.wip/js/plugins/photoswipe.esm.js'
+});
+lightbox.init();
+</script>
+<div class="pswp-gallery pswp-gallery--single-column" id="gallery--simple">
+  <a href="https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-2500.jpg" 
+    data-pswp-width="1669" 
+    data-pswp-height="2500" 
+    target="_blank">
+    <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-200.jpg" alt="" />
+  </a>
+
+  <!-- cropped thumbnail -->
+  <a href="https://cdn.photoswipe.com/photoswipe-demo-images/photos/7/img-2500.jpg" 
+    data-pswp-width="1875" 
+    data-pswp-height="2500" 
+    data-cropped="true" 
+    target="_blank">
+    <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/7/img-200.jpg" alt="" />
+    Cropped
+  </a>
+  
+  <!-- data-pswp-src with custom URL in href -->
+  <a href="https://unsplash.com" 
+    data-pswp-src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg"
+    data-pswp-width="2500" 
+    data-pswp-height="1666" 
+    target="_blank">
+    <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg" alt="" />
+  </a>
+
+  <!-- Without thumbnail -->
+  <a href="http://example.com" 
+    data-pswp-src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/5/img-2500.jpg"
+    data-pswp-width="2500" 
+    data-pswp-height="1668" 
+    target="_blank">
+    No thumbnail
+  </a>
+
+  <!-- wrapped with any element -->
+  <div>
+    <a href="https://cdn.photoswipe.com/photoswipe-demo-images/photos/6/img-2500.jpg"
+      data-pswp-width="2500" 
+      data-pswp-height="1667" 
+      target="_blank">
+      <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/6/img-200.jpg" alt="" />
+    </a>
+  </div>
+</div>
                 <div class="c-article c-article--compressed-b">
                     <div class="o-article-grid o-article-grid--with-form">
                         <div class="o-article-grid__column-1">
