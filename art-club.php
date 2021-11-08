4c1ee8dc5cd9219e8280cdf1f8da889ff6b00d2d
Jay George <github@jaygeorge.co.uk>
2021-11-08 09:15:41 +0000

    Remove photoswipe

--- art-club.php
+++ art-club.php
@@ -103,70 +103,7 @@
                 </figure>
             </div>
             <div class="u-width-max-1">
-                <?php $filename = 'plugins/photoswipe'; include '_inc/_stylesheet.php'; ?>
-                <script type="module">
-// Include Lightbox 
-import PhotoSwipeLightbox from '/js/plugins/photoswipe.esm.js';
-
-const lightbox = new PhotoSwipeLightbox({
-  // may select multiple "galleries"
-  gallery: '#gallery--simple',
-
-  // Elements within gallery (slides)
-  children: 'a',
-
-  // Include PhotoSwipe Core
-  // and use absolute path (that starts with http(s)://)
-  pswpModule: 'https://thesybarite-static.wip/js/plugins/photoswipe.esm.js'
-});
-lightbox.init();
-</script>
-<div class="pswp-gallery pswp-gallery--single-column" id="gallery--simple">
-  <a href="https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-2500.jpg" 
-    data-pswp-width="1669" 
-    data-pswp-height="2500" 
-    target="_blank">
-    <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/2/img-200.jpg" alt="" />
-  </a>
-
-  <!-- cropped thumbnail -->
-  <a href="https://cdn.photoswipe.com/photoswipe-demo-images/photos/7/img-2500.jpg" 
-    data-pswp-width="1875" 
-    data-pswp-height="2500" 
-    data-cropped="true" 
-    target="_blank">
-    <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/7/img-200.jpg" alt="" />
-    Cropped
-  </a>
-  
-  <!-- data-pswp-src with custom URL in href -->
-  <a href="https://unsplash.com" 
-    data-pswp-src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-2500.jpg"
-    data-pswp-width="2500" 
-    data-pswp-height="1666" 
-    target="_blank">
-    <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/3/img-200.jpg" alt="" />
-  </a>
-
-  <!-- Without thumbnail -->
-  <a href="http://example.com" 
-    data-pswp-src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/5/img-2500.jpg"
-    data-pswp-width="2500" 
-    data-pswp-height="1668" 
-    target="_blank">
-    No thumbnail
-  </a>
-
-  <!-- wrapped with any element -->
-  <div>
-    <a href="https://cdn.photoswipe.com/photoswipe-demo-images/photos/6/img-2500.jpg"
-      data-pswp-width="2500" 
-      data-pswp-height="1667" 
-      target="_blank">
-      <img src="https://cdn.photoswipe.com/photoswipe-demo-images/photos/6/img-200.jpg" alt="" />
-    </a>
-  </div>
-</div>
+                
                 <div class="c-article c-article--compressed-b">
                     <div class="o-article-grid o-article-grid--with-form">
                         <div class="o-article-grid__column-1">
